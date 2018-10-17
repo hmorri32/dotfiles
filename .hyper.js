4d48eee79468c3e2c453ac2d825198c2d03233d2
Hugh Morrison <hmorri32@gmail.com>
2018-10-17 11:18:27 -0600

    chic

--- .hyper.js
+++ .hyper.js
@@ -6,31 +6,42 @@ module.exports = {
   config: {
     // Choose either "stable" for receiving highly polished,
     // or "canary" for less polished but more frequent updates
-    updateChannel: 'stable',
-    fontSize: 13,
+    updateChannel: 'canary',
+    fontSize: 12,
     fontFamily: '"Meslo LG S for Powerline"',
     // fontFamily: '"Inconsolata-g for Powerline", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
-    cursorColor: 'rgba(248,28,229,0.8)',
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
     cursorShape: 'BLOCK',
     cursorBlink: false,
     foregroundColor: '#fff',
-    backgroundColor: '#000',
-    cursorColor: 'rgba(255,255,255, 0.9)',
-
-    // border color (window, tabs)
-    borderColor: '#333',
+    // backgroundColor: '#000',
+	cursorColor: '#fffff',
+	selectionColor: 'rgba(224, 239, 254,0.3)',
 
     // custom css to embed in the main window
-    css: '',
+    css: `
+      .terms_19b1ydh {margin-top: 20px;}
+      .header_windowHeader {
+        display: none;
+      }
+      .tabs_nav {
+        top: 0;
+      }
+      .terms_terms {
+        margin-top: 0;
+      }
+      .terms_termsShifted {
+        margin-top: 30px;
+      }
+    `,
 
     // custom css to embed in the terminal window
-    termCSS: `
-    x-screen x-row {
-      line-height: initial;
-    }
-  `,
+  //   termCSS: `
+  //   x-screen x-row {
+  //     line-height: initial;
+  //   }
+  // `,
 
     // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
     // default: `false` on Linux, `true` on Windows (ignored on macOS)
@@ -42,29 +53,31 @@ module.exports = {
     showWindowControls: '',
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '3px 0px 20px 3px',
+    padding: '12px 3px 10px 5px',
+    // padding: '12px 14px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
-    colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
-    },
+    // colors: {
+    //   black: '#000000',
+    //   red: '#ff0000',
+    //   green: '#33ff00',
+    //   yellow: '#ffff00',
+    //   blue: '#0066ff',
+    //   magenta: '#cc00ff',
+    //   cyan: '#00ffff',
+    //   white: '#d0d0d0',
+    //   lightBlack: '#808080',
+    //   lightRed: '#ff0000',
+    //   lightGreen: '#33ff00',
+    //   lightYellow: '#ffff00',
+    //   lightBlue: '#0066ff',
+    //   lightMagenta: '#cc00ff',
+    //   lightCyan: '#00ffff',
+    //   lightWhite: '#ffffff',
+    // },
+
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
@@ -78,7 +91,7 @@ module.exports = {
     //
     // Powershell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
+    shell: 'zsh',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
     // by default ['--login'] will be used
@@ -104,34 +117,34 @@ module.exports = {
           up: 'command+alt+up',
           down: 'command+alt+down',
           left: 'command+alt+left',
-          right: 'command+alt+right'
+          right: 'command+alt+right',
         },
         jump_prefix: 'command+alt', // completed with 1-9 digits
       },
       showIndicators: false, // Show pane number
     },
-    hyperclean: {
-      hideTabs: true, // default: false
+    hyperline: {
+      plugins: ['ip', 'memory', 'cpu', 'network', 'battery'],
     },
+	confirmQuit: true,
   },
 
   // a list of plugins to fetch and install from npm
   // format: [@org/]project[#version]
   plugins: [
-    'hyperterm-hipster',
-    // 'hyper-unity',
-    // 'hyper-snazzy',
-    // 'hyperterm-material',
-    // 'hyper-clean',
-    // 'hyper-ayu',
-    // 'an-old-hype',
     'hypercwd',
     'hyper-simple-highlight-active-session',
-    'hyperclean',
+    // 'hyper-altair',
+    // 'hyperterm-monokai',
+    // 'hyper-ayu-mirage',
+    // 'hyper-captain-sweetheart',
+    'hyperterm-chesterish',
     'hyper-pane',
     'hyper-search',
     'hyperterm-cursor',
-    'hyperline#0.6.1'
+    'hyperline',
+    'hyper-confirm',
+    'hyper-final-say',
   ],
 
   // in development, you can create a directory under
